6ce99ed8469eccacc1fb162a5f0c016c5538d14c
Hew Wolff <hewwolff@sonic.net>
2015-04-04 12:56:33 -0700

    add failing test

--- tagging/test/integration/tags_test.rb
+++ tagging/test/integration/tags_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class TagsTest < ActionDispatch::IntegrationTest
   test 'get before set' do
-    get '/tags/thing/123'
+    get '/tags/thing/abc'
     assert_response :missing
+  end
+
+  test 'get and set' do
+    post '/tag', '{"entity_type": "thing", "entity_id": "abc", "tags": ["nice", "cool"]}', CONTENT_TYPE: 'application/json'
+    get '/tags/thing/abc'
+    assert_response :success
   end
 end
