a3a24732d5fdf2fdd644462cfcf92f11d03f71eb
Hew Wolff <hewwolff@sonic.net>
2015-04-04 13:45:48 -0700

    refactor

--- tagging/test/integration/tags_test.rb
+++ tagging/test/integration/tags_test.rb
@@ -1,32 +1,36 @@
 require 'test_helper'
 
 class TagsTest < ActionDispatch::IntegrationTest
-  test 'entity get and set' do
+  setup do
     post '/tag', '{"entity_type": "thing", "entity_id": "abc", "tags": ["nice", "cool"]}', CONTENT_TYPE: 'application/json'
     assert_response :success
+  end
+
+  test 'entity get' do
     get '/tags/thing/abc'
     assert_response :success
-    expected = {
+    assert_json_response({
       'entity_type' => 'thing',
       'entity_id' => 'abc',
       'tags' => ['cool', 'nice']
-    }
-    assert_equal expected, JSON.parse(response.body)
+    })
 
     get '/tags/thing/xyz'
     assert_response :missing
   end
 
   test 'replacing entity' do
-    post '/tag', '{"entity_type": "thing", "entity_id": "abc", "tags": ["nice"]}', CONTENT_TYPE: 'application/json'
-    post '/tag', '{"entity_type": "thing", "entity_id": "abc", "tags": ["cool"]}', CONTENT_TYPE: 'application/json'
+    post '/tag', '{"entity_type": "thing", "entity_id": "abc", "tags": ["awesome"]}', CONTENT_TYPE: 'application/json'
     get '/tags/thing/abc'
 
-    expected = {
+    assert_json_response({
       'entity_type' => 'thing',
       'entity_id' => 'abc',
-      'tags' => ['cool']
-    }
-    assert_equal expected, JSON.parse(response.body)
+      'tags' => ['awesome']
+    })
+  end
+
+  def assert_json_response(hash)
+    assert_equal hash, JSON.parse(response.body)
   end
 end
