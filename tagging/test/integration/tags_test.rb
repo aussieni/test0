2fbbc4698b52c1b601159b016748ade66ea11e1e
Hew Wolff <hewwolff@sonic.net>
2015-04-04 13:14:13 -0700

    refactor

--- tagging/test/integration/tags_test.rb
+++ tagging/test/integration/tags_test.rb
@@ -1,26 +1,18 @@
 require 'test_helper'
 
 class TagsTest < ActionDispatch::IntegrationTest
-  test 'get before set' do
-    get '/tags/thing/abc'
-    assert_response :missing
-  end
-
   test 'entity get and set' do
     post '/tag', '{"entity_type": "thing", "entity_id": "abc", "tags": []}', CONTENT_TYPE: 'application/json'
     assert_response :success
     get '/tags/thing/abc'
     assert_response :success
-    # !!! check response
+    # !!! check response content
+
+    get '/tags/thing/xyz'
+    assert_response :missing
   end
 
 =begin
-  test 'get and set' do
     post '/tag', '{"entity_type": "thing", "entity_id": "abc", "tags": ["nice", "cool"]}', CONTENT_TYPE: 'application/json'
-    assert_response :success
-    get '/tags/thing/abc'
-    assert_response :success
-    # !!! check response
-  end
 =end
 end
